c41ad8617264a2d7b44c3e540ecacc58154dcc26
Jakob A <jdanders1999@gmail.com>
2023-12-13 22:37:18 -0500

    deleted vanilla code, added spaces to clean up code, added start button variable to get functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-
 // page elements
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
 var questionEL = document.getElementById("question");
+
 // global variables
 var time = 90;
 var questionNumber = 0;
 var timeVar;
 var questionsCorrect = 0;
 var userScore;
+
+// button variable
+var startButton = document.getElementById("startButton");
+
 // Variables used to create new "pages"
 var welcomeScreenEl = document.getElementById("welcomeScreen");
 var questionScreenEl = document.getElementById("questionScreen");
@@ -17,124 +21,125 @@ var scoreScreenEl = document.getElementById("scoreScreen");
 
 //event listeners
 startButton.addEventListener("click", startQuiz);
-
+optionsEl.addEventListener("click", chooseOption);
 
 function startQuiz() {
-    welcomeScreenEl.setAttribute("class", "hidden");
-    returnHomeEl.removeAttribute("class");
-    questionScreenEl.removeAttribute("class");
-    timeVar = setInterval(timerCountdown, 1000);
-    timeLeftEl.textContent = time;
-    displayQuestion();
-};
+  welcomeScreenEl.setAttribute("class", "hidden");
+  returnHomeEl.removeAttribute("class");
+  questionScreenEl.removeAttribute("class");
+  timeVar = setInterval(timerCountdown, 1000);
+  timeLeftEl.textContent = time;
+  displayQuestion();
+}
 
 // timer countdown function
 function timerCountdown() {
-    time--;
-    timeLeftEl.textContent = time;
+  time--;
+  timeLeftEl.textContent = time;
 
-    if (seconds <= 0) {
-        scoreScreen();
-    };
-
+  if (seconds <= 0) {
+    scoreScreen();
+  }
 }
+
 // writes out the question
 function displayQuestion() {
-    var questionInfo = questions[questionNumber];
-    var questionEL = document.getElementById("question");
-    questionEL.textContent = questionInfo.question;
+  var questionInfo = questions[questionNumber];
+  var questionEL = document.getElementById("question");
+  questionEL.textContent = questionInfo.question;
 
-    //erases previous question
-optionsEL.innerHTML = '';
+  //erases previous question
+  optionsEL.innerHTML = "";
 
-// gives you the question
-for(var i=0; i<questions.length; i++){
+  // gives you the question
+  for (var i = 0; i < questions.length; i++) {
     answers = [];
-var option = questionInfo.options[i]; 
-var optionButton = document.createElement("button");
-optionButton.setAttribute("class", "option");
-optionButton.setAttribute("value", option);
+    var option = questionInfo.options[i];
+    var optionButton = document.createElement("button");
+    optionButton.setAttribute("class", "option");
+    optionButton.setAttribute("value", option);
 
-optionButton.textContent = i + 1 + ". " + option;
+    optionButton.textContent = i + 1 + ". " + option;
 
-optionsEl.appendChild(optionButton);
-};
+    optionEL.appendChild(optionButton);
+  }
 };
 
 function chooseOption(event) {
-    var buttonEl = event.target;
-    if (!buttonEl.matches(".option")) {
-        return;
-    };
-    if (buttonEl.value !== questions[questionNumber].answer) {
-        time -= 6;
-        if (time <= 0) {
-            time = 0;
-        }
-        responseEl.setAttribute("class", "incorrect");
-        responseEl.textContent = "Incorrect!";
+  var buttonEl = event.target;
+  if (!buttonEl.matches(".option")) {
+    return;
+  };
+
+  if (buttonEl.value !== questions[questionNumber].answer) {
+    time -= 6;
+    if (time <= 0) {
+      time = 0;
     }
- else {
-        questionsCorrect++;
+    
+    responseEl.setAttribute("class", "incorrect");
+    responseEl.textContent = "Incorrect!";
+  } 
+  
+  else {
+    questionsCorrect++;
 
-        responseEl.setAttribute("class", "correct");
-        responseEl.textContent = "Correct!";
-    };
-    setTimeout(function () {
-        responseEl.setAttribute("class", "hidden");
-    }, 1000);
-    questionNumber++;
-    userScore = time * questionsCorrect;
+    responseEl.setAttribute("class", "correct");
+    responseEl.textContent = "Correct!";
+  };
 
-userAnswer = (answerContainers[i].querySelecter('input[name.question'+i+']:checked')||{}).value;
-if(time <= 0 || questionNumber === questions.length) {
-scoreScreen();
-    answerContainers[i].style.color = 'green';
-}
-
-else{
+  if (time <= 0 || questionNumber === questions.length) {
+    scoreScreen();
+    answerContainers[i].style.color = "green";
+  } else {
     displayQuestion();
-    answerContainers[i].style.color = 'red'
-}
+    answerContainers[i].style.color = "red";
+  }
 };
+
 function scoreScreen() {
-    clearInterval(timeVar);
-    questionScreenEl.setAttribute("class", "hidden");
-    scoreScreenEl.removeAttribute("class");
-    var scoreEL = document.getElementById("results");
+  clearInterval(timeVar);
+  questionScreenEl.setAttribute("class", "hidden");
+  scoreScreenEl.removeAttribute("class");
+  var scoreEL = document.getElementById("results");
 }
 
-submitButton.onclick = function(){
-    showResults(questions, quizContainer, resultsContainer);
-}
+submitButton.onclick = function () {
+  showResults(questions, quizContainer, resultsContainer);
+};
 
-submitButton.onclick = function(){
-	showResults(questions, quizContainer, resultsContainer);
-}
+submitButton.onclick = function () {
+  showResults(questions, quizContainer, resultsContainer);
+};
 
 var questions = [
-    {
-        question:'Which of the following elements is not an html element?', 
-         answers:['span', 'div', 'h7', 'body'], 
-         correctAnswer: 'h7'},
+  {
+    question: "Which of the following elements is not an html element?",
+    answers: ["span", "div", "h7", "body"],
+    correctAnswer: "h7",
+  },
 
-    {
-        question:'Which of the following would you use to create an array?', 
-        answers:['< >', '[ ]', '{ }', '( )'], 
-        correctAnswer: '[ ]'},
+  {
+    question: "Which of the following would you use to create an array?",
+    answers: ["< >", "[ ]", "{ }", "( )"],
+    correctAnswer: "[ ]",
+  },
 
-    {
-        question:'How many different header tags can you make?', 
-         answers:['2', '3', '5', '6'], 
-         correctAnswer: '6'},
+  {
+    question: "How many different header tags can you make?",
+    answers: ["2", "3", "5", "6"],
+    correctAnswer: "6",
+  },
 
-    {
-        question:'What is the correct term for true/false statements?', 
-          answers:['boolean', 'string', 'numbers', 'span'], 
-          correctAnswer: 'boolean'},
+  {
+    question: "What is the correct term for true/false statements?",
+    answers: ["boolean", "string", "numbers", "span"],
+    correctAnswer: "boolean",
+  },
 
-    {
-        question:'What can you do if you dont understand this?', 
-         answers:['Google', 'Tutor', 'Ask a peer', 'All of the Above'], 
-         correctAnswer: 'All of the Above'}
+  {
+    question: "What can you do if you dont understand this?",
+    answers: ["Google", "Tutor", "Ask a peer", "All of the Above"],
+    correctAnswer: "All of the Above",
+  },
 ];

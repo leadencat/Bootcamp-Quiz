34c05253b5049dd07405ccc5caa6530deb5f2173
Jakob A <jdanders1999@gmail.com>
2023-12-13 22:23:29 -0500

    updated css to flow with js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var questionEL = document.getElementById("question");
 // global variables
 var time = 90;
 var questionNumber = 0;
@@ -12,9 +13,104 @@ var userScore;
 // Variables used to create new "pages"
 var welcomeScreenEl = document.getElementById("welcomeScreen");
 var questionScreenEl = document.getElementById("questionScreen");
+var scoreScreenEl = document.getElementById("scoreScreen");
+
 //event listeners
 startButton.addEventListener("click", startQuiz);
 
+
+function startQuiz() {
+    welcomeScreenEl.setAttribute("class", "hidden");
+    returnHomeEl.removeAttribute("class");
+    questionScreenEl.removeAttribute("class");
+    timeVar = setInterval(timerCountdown, 1000);
+    timeLeftEl.textContent = time;
+    displayQuestion();
+};
+
+// timer countdown function
+function timerCountdown() {
+    time--;
+    timeLeftEl.textContent = time;
+
+    if (seconds <= 0) {
+        scoreScreen();
+    };
+
+}
+// writes out the question
+function displayQuestion() {
+    var questionInfo = questions[questionNumber];
+    var questionEL = document.getElementById("question");
+    questionEL.textContent = questionInfo.question;
+
+    //erases previous question
+optionsEL.innerHTML = '';
+
+// gives you the question
+for(var i=0; i<questions.length; i++){
+    answers = [];
+var option = questionInfo.options[i]; 
+var optionButton = document.createElement("button");
+optionButton.setAttribute("class", "option");
+optionButton.setAttribute("value", option);
+
+optionButton.textContent = i + 1 + ". " + option;
+
+optionsEl.appendChild(optionButton);
+};
+};
+
+function chooseOption(event) {
+    var buttonEl = event.target;
+    if (!buttonEl.matches(".option")) {
+        return;
+    };
+    if (buttonEl.value !== questions[questionNumber].answer) {
+        time -= 6;
+        if (time <= 0) {
+            time = 0;
+        }
+        responseEl.setAttribute("class", "incorrect");
+        responseEl.textContent = "Incorrect!";
+    }
+ else {
+        questionsCorrect++;
+
+        responseEl.setAttribute("class", "correct");
+        responseEl.textContent = "Correct!";
+    };
+    setTimeout(function () {
+        responseEl.setAttribute("class", "hidden");
+    }, 1000);
+    questionNumber++;
+    userScore = time * questionsCorrect;
+
+userAnswer = (answerContainers[i].querySelecter('input[name.question'+i+']:checked')||{}).value;
+if(time <= 0 || questionNumber === questions.length) {
+scoreScreen();
+    answerContainers[i].style.color = 'green';
+}
+
+else{
+    displayQuestion();
+    answerContainers[i].style.color = 'red'
+}
+};
+function scoreScreen() {
+    clearInterval(timeVar);
+    questionScreenEl.setAttribute("class", "hidden");
+    scoreScreenEl.removeAttribute("class");
+    var scoreEL = document.getElementById("results");
+}
+
+submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+}
+
+submitButton.onclick = function(){
+	showResults(questions, quizContainer, resultsContainer);
+}
 
 var questions = [
     {
@@ -42,72 +138,3 @@ var questions = [
          answers:['Google', 'Tutor', 'Ask a peer', 'All of the Above'], 
          correctAnswer: 'All of the Above'}
 ];
-
-startButton.addEventListener('click', function() {
-    timeLeftEl.textContent = seconds;
-    startScreen.setAttribute('class', 'hidden');
-    questionEl.removeAttribute('class');
-    displayQ();
-});
-
-// timer countdown function
-function timerCountdown() {
-    time--;
-    timeLeftEl.textContent = time;
-
-    if (seconds <= 0) {
-        scoreScreen();
-    }
-}
-var output = [];
-var answers;
-
-for(var i=0; i<questions.length; i++){
-    answers = [];
-
-    for(letter in questions[i].answers){
-        answers.push(
-            '<label>'
-                + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                + letter + ': '
-                + questions[i].answers[letter]
-            + '</label>'
-        );
-    }
-    output.push(
-        '<div class="question">' + questions[i].question + '</div>'
-        + '<div class="answers">' + answers.join('') + '</div>'
-		);
-	}
-
-	quizContainer.innerHTML = output.join('');
-
-function showResults(questions, quizContainer, resultsContainer) {
-var answerContainers = quizContainer.querySelecterAll('.answers');
-var userAnswer = '';
-var numCorrect = 0;
-
-for(var i=0; i<questions.length; i++){
-
-userAnswer = (answerContainers[i].querySelecter('input[name.question'+i+']:checked')||{}).value;
-if(userAnswer===questions[i].correctAnswer){
-    numCorrect++;
-    answerContainers[i].style.color = 'green';
-}
-else{
-    answerContainers[i].style.color = 'red'
-}
-resultsContainer.innerHTML = numCorrect + 'out of' + questions.length;
-}
-
-}
-showQuestions(questions, quizContainer);
-
-
-submitButton.onclick = function(){
-    showResults(questions, quizContainer, resultsContainer);
-}
-
-submitButton.onclick = function(){
-	showResults(questions, quizContainer, resultsContainer);
-}
